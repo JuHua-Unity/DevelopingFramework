dfc02c8a08552096213010d9d3ad740cd443fc00
juhua <w1762405771@163.com>
2020-05-16 12:29:48 +0800

    no message

---
+++ Unity/Assets/ThirdParty/ShowLog/Runtime/GUITest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace ETHotfix
+{
+    public class GUITest : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Window(0, new Rect(0, 0, Screen.width, Screen.height), SSS, "");
+        }
+
+        private void SSS(int id)
+        {
+            GUILayout.BeginScrollView(new Vector2(100, 100));
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.Label("11111111111111111111");
+            GUILayout.EndScrollView();
+        }
+    }
+}
